1ae5c12d3c70475d58d71b7e4ecacd26029e8621
Azamat Mambetov <zikbest11@yandex.ru>
2025-02-10 21:51:17 +0300

    chore(db): remove migrations

--- apps/gateway/prisma/migrations/20240419164601_change_photo_column/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - A unique constraint covering the columns `[userId]` on the table `photos` will be added. If there are existing duplicate values, this will fail.
-
-*/
--- CreateIndex
-CREATE UNIQUE INDEX "photos_userId_key" ON "photos"("userId");
