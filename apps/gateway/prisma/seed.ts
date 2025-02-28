762efe20e2f1488e13d6d4956a030f346c23f92f
Azamat Mambetov <zikbest11@yandex.ru>
2025-02-28 21:09:12 +0300

    chore: change seeds and schema

--- apps/gateway/prisma/seed.ts
+++ apps/gateway/prisma/seed.ts
@@ -5,34 +5,25 @@ import { SocialLinkType } from '../src/common/enums';
 const prisma = new PrismaClient();
 
 async function main() {
-  await prisma.socialLinkType.createMany({
+  await prisma.user.createMany({
     data: [
       {
-        id: SocialLinkType.Facebook,
-        name: SocialLinkType[SocialLinkType.Facebook],
+        fullName: 'Stephen King',
+        bio: 'Stephen Edwin King is an American author. Called the "King of Horror", he has also explored other genres, among them suspense, crime, science-fiction, fantasy and mystery. He has also written approximately 200 short stories, most of which have been published in collections.',
+        email: 'stephen.king@oclio.com',
+        hashedPassword:
+          '$2a$12$B1/RVvEjeXFOGBP9gnRMF.rnquFD7bvQN51NdYMc2wvgTNatLBeBO',
+        preferences: { theme: 'white' },
       },
       {
-        id: SocialLinkType.Twitter,
-        name: SocialLinkType[SocialLinkType.Twitter],
-      },
-      {
-        id: SocialLinkType.Instagram,
-        name: SocialLinkType[SocialLinkType.Instagram],
-      },
-      {
-        id: SocialLinkType.Telegram,
-        name: SocialLinkType[SocialLinkType.Telegram],
-      },
-      {
-        id: SocialLinkType.TikTok,
-        name: SocialLinkType[SocialLinkType.TikTok],
-      },
-      {
-        id: SocialLinkType.YouTube,
-        name: SocialLinkType[SocialLinkType.YouTube],
+        fullName: 'George Orwell',
+        bio: 'Eric Arthur Blair was a British novelist, poet, essayist, journalist, and critic who wrote under the pen name of George Orwell. His work is characterised by lucid prose, social criticism, opposition to all totalitarianism, and support of democratic socialism.',
+        email: 'george.orwell@oclio.com',
+        hashedPassword:
+          '$2a$12$B1/RVvEjeXFOGBP9gnRMF.rnquFD7bvQN51NdYMc2wvgTNatLBeBO',
+        preferences: { theme: 'dark' },
       },
     ],
-    skipDuplicates: true,
   });
 }
 
