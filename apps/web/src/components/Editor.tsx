c32053ad127a347a65ea34f4e1538638e106a6b8
Azamat Mambetov <zikbest11@yandex.ru>
2025-03-16 13:56:56 +0300

    chore: add tiptop editor

---
+++ apps/web/src/components/Editor.tsx
@@ -0,0 +1,28 @@
+import {
+  FloatingMenu,
+  BubbleMenu,
+  useEditor,
+  EditorContent,
+} from "@tiptap/react";
+import StarterKit from "@tiptap/starter-kit";
+
+const extensions = [StarterKit];
+
+const content = "<p>Hello World!</p>";
+
+const Editor = () => {
+  const editor = useEditor({
+    extensions,
+    content,
+  });
+
+  return (
+    <>
+      <EditorContent editor={editor} />
+      <FloatingMenu editor={editor}>This is the floating menu</FloatingMenu>
+      <BubbleMenu editor={editor}>This is the bubble menu</BubbleMenu>
+    </>
+  );
+};
+
+export default Editor;
--- apps/web/src/pages/studio/Studio.tsx
+++ apps/web/src/pages/studio/Studio.tsx
@@ -1,9 +1,18 @@
 import { useParams } from "react-router-dom";
+import StudioLayout from "@/layouts/StudioLayout";
+import Editor from "@/components/Editor";
 
 const Studio = () => {
   const { bookId } = useParams();
 
-  return <div>Studio {bookId}</div>;
+  return (
+    <StudioLayout>
+      <div>Studio {bookId}</div>
+      <div className="flex h-full w-full">
+        <Editor />
+      </div>
+    </StudioLayout>
+  );
 };
 
 export default Studio;
